3f80544e70804f3ebe0108937c72aafa52ee6e50
Mike Kostenko <vopros.m@gmail.com>
2024-12-03 12:39:28 +0000

    Day 03.

--- eu.singledev.aoc2024/src/main/java/com/company/Day03.java
+++ eu.singledev.aoc2024/src/main/java/com/company/Day03.java
@@ -9,6 +9,8 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author Mike Kostenko on 02/12/2024
@@ -17,6 +19,7 @@ import java.util.List;
 @NoArgsConstructor
 public class Day03 {
     String filePath;
+    List<String> input = new ArrayList<>();
 
     public void setFilePath(String filePath) {
         this.filePath = filePath;
@@ -27,20 +30,44 @@ public class Day03 {
     private void initiateDataStructures() {
         log.info("Initiating data structures...");
         try {
-            List<String> lines = Files.readAllLines(Paths.get(filePath));
-
+            input.addAll(Files.readAllLines(Paths.get(filePath)));
         } catch (IOException e) {
             log.error("An error occurred while reading the file: {}", e.getMessage(), e);
         }
     }
 
-    public int calculatePart1() {
+    public long calculatePart1() {
         log.info("Calculating Part 1...");
-        return 0;
+        long result = 0;
+        for (String line: input) {
+            Pattern pattern = Pattern.compile("mul\\((\\d{1,3}),(\\d{1,3})\\)");
+            Matcher matcher = pattern.matcher(line);
+            while (matcher.find()) {
+                int a = Integer.parseInt(matcher.group(1));
+                int b = Integer.parseInt(matcher.group(2));
+                result += (long) a * b;
+            }
+        }
+        return result;
     }
 
-    public int calculatePart2() {
+    public long calculatePart2() {
         log.info("Calculating Part 2...");
-        return 0;
+        long result = 0;
+        String line = String.join("", input);
+        String[] parts = line.split("do\\(\\)");
+        for (String part: parts) {
+            int index = part.indexOf("don't()");
+            String searchScope = part.substring(0, index == -1 ? part.length() : index);
+            Pattern pattern = Pattern.compile("mul\\((\\d{1,3}),(\\d{1,3})\\)");
+            Matcher matcher = pattern.matcher(searchScope);
+            while (matcher.find()) {
+                int a = Integer.parseInt(matcher.group(1));
+                int b = Integer.parseInt(matcher.group(2));
+                result += (long) a * b;
+            }
+        }
+
+        return result;
     }
 }
--- eu.singledev.aoc2024/src/main/java/com/company/Main.java
+++ eu.singledev.aoc2024/src/main/java/com/company/Main.java
@@ -5,10 +5,10 @@ public class Main {
     public static void main(String[] args) {
         if (args.length > 0) {
             String filePath = args[0];
-            Day02 day02 = new Day02();
-            day02.setFilePath(filePath);
-            System.out.println(day02.calculatePart1()); // 334
-            System.out.println(day02.calculatePart2()); // 400
+            Day03 day03 = new Day03();
+            day03.setFilePath(filePath);
+            System.out.println(day03.calculatePart1()); // 188116424
+            System.out.println(day03.calculatePart2()); // 104245808
         }
     }
 }
