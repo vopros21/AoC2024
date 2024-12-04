0449071c1027c4b1bff1300b763489931399e71e
Mike Kostenko <vopros.m@gmail.com>
2024-12-04 12:20:11 +0000

    Day 04.

--- eu.singledev.aoc2024/src/main/java/com/company/Day04.java
+++ eu.singledev.aoc2024/src/main/java/com/company/Day04.java
@@ -21,11 +21,11 @@ public class Day04 {
     public void setFilePath(String filePath) {
         this.filePath = filePath;
         initiateDataStructures();
-        log.info("Finish initialization.");
+        System.out.println("Finish initialization.");
     }
 
     private void initiateDataStructures() {
-        log.info("Initiating data structures...");
+        System.out.println("Initiating data structures...");
         try {
             input.addAll(Files.readAllLines(Paths.get(filePath)));
         } catch (IOException e) {
@@ -34,10 +34,126 @@ public class Day04 {
     }
 
     public int calculatePart1() {
-        return 0;
+        int result = 0;
+        for (int i = 0; i < input.size(); i++) {
+            for (int j = 0; j < input.get(i).length(); j++) {
+                if (input.get(i).charAt(j) == 'X') {
+                    result += searchXMAS(i, j);
+                }
+            }
+        }
+        return result;
+    }
+
+    private int searchXMAS(int i, int j) {
+        int result = 0;
+        for (Direction direction : Direction.values()) {
+            int y = i;
+            int x = j;
+            switch (direction) {
+                case E:
+                    if (++x < input.get(y).length() && input.get(y).charAt(x) != 'M') {
+                        continue;
+                    } else if (++x < input.get(y).length() && input.get(y).charAt(x) != 'A') {
+                        continue;
+                    } else if (++x < input.get(y).length() && input.get(y).charAt(x) == 'S') {
+                        result++;
+                    }
+                    break;
+                case W:
+                    if (--x > 0 && input.get(y).charAt(x) != 'M') {
+                        continue;
+                    } else if (--x > 0 && input.get(y).charAt(x) != 'A') {
+                        continue;
+                    } else if (--x >= 0 && input.get(y).charAt(x) == 'S') {
+                        result++;
+                    }
+                    break;
+                case N:
+                    if (--y > 0 && input.get(y).charAt(x) != 'M') {
+                        continue;
+                    } else if (--y > 0 && input.get(y).charAt(x) != 'A') {
+                        continue;
+                    } else if (--y >= 0 && input.get(y).charAt(x) == 'S') {
+                        result++;
+                    }
+                    break;
+                case S:
+                    if (++y < input.size() && input.get(y).charAt(x) != 'M') {
+                        continue;
+                    } else if (++y < input.size() && input.get(y).charAt(x) != 'A') {
+                        continue;
+                    } else if (++y < input.size() && input.get(y).charAt(x) == 'S') {
+                        result++;
+                    }
+                    break;
+                case NE:
+                    if (--y > 0 && ++x < input.get(y).length() && input.get(y).charAt(x) != 'M') {
+                        continue;
+                    } else if (--y > 0 && ++x < input.get(y).length() && input.get(y).charAt(x) != 'A') {
+                        continue;
+                    } else if (--y >= 0 && ++x < input.get(y).length() && input.get(y).charAt(x) == 'S') {
+                        result++;
+                    }
+                    break;
+                case NW:
+                    if (--y > 0 && --x > 0 && input.get(y).charAt(x) != 'M') {
+                        continue;
+                    } else if (--y > 0 && --x > 0 && input.get(y).charAt(x) != 'A') {
+                        continue;
+                    } else if (--y >= 0 && --x >= 0 && input.get(y).charAt(x) == 'S') {
+                        result++;
+                    }
+                    break;
+                case SE:
+                    if (++y < input.size() && ++x < input.get(y).length() && input.get(y).charAt(x) != 'M') {
+                        continue;
+                    } else if (++y < input.size() && ++x < input.get(y).length() && input.get(y).charAt(x) != 'A') {
+                        continue;
+                    } else if (++y < input.size() && ++x < input.get(y).length() && input.get(y).charAt(x) == 'S') {
+                        result++;
+                    }
+                    break;
+                case SW:
+                    if (++y < input.size() && --x > 0 && input.get(y).charAt(x) != 'M') {
+                        break;
+                    } else if (++y < input.size() && --x > 0 && input.get(y).charAt(x) != 'A') {
+                        break;
+                    } else if (++y < input.size() && --x >= 0 && input.get(y).charAt(x) == 'S') {
+                        result++;
+                    }
+            }
+        }
+        return result;
     }
 
     public int calculatePart2() {
-        return 0;
+        int result = 0;
+        for (int i = 0; i < input.size(); i++) {
+            for (int j = 0; j < input.get(i).length(); j++) {
+                if (input.get(i).charAt(j) == 'A') {
+                    result += searchMAS(i, j);
+                }
+            }
+        }
+        return result;
+    }
+
+    private int searchMAS(int i, int j) {
+        boolean l2r = false;
+        boolean r2l = false;
+        if (i > 0 && j > 0 && i < input.size() - 1 && j < input.get(i).length() - 1) {
+            if ((input.get(i - 1).charAt(j - 1) == 'M' && input.get(i + 1).charAt(j + 1) == 'S') || (input.get(i - 1).charAt(j - 1) == 'S' && input.get(i + 1).charAt(j + 1) == 'M')) {
+                l2r = true;
+            }
+            if ((input.get(i - 1).charAt(j + 1) == 'M' && input.get(i + 1).charAt(j - 1) == 'S') || (input.get(i - 1).charAt(j + 1) == 'S' && input.get(i + 1).charAt(j - 1) == 'M')) {
+                r2l = true;
+            }
+        }
+        return l2r && r2l ? 1 : 0;
+    }
+
+    private enum Direction {
+        N, NE, E, SE, S, SW, W, NW
     }
 }
