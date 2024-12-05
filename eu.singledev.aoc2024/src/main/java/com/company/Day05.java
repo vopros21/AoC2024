c77aa9ab1d6bb11d6aa25f5e9a9f745fd506e223
Mike Kostenko <vopros.m@gmail.com>
2024-12-05 13:56:28 +0000

    Day 05.

--- eu.singledev.aoc2024/src/main/java/com/company/Day05.java
+++ eu.singledev.aoc2024/src/main/java/com/company/Day05.java
@@ -5,8 +5,8 @@ import lombok.NoArgsConstructor;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * @author Mike Kostenko on 05/12/2024
@@ -15,6 +15,9 @@ import java.util.List;
 public class Day05 {
     String filePath;
     List<String> input = new ArrayList<>();
+    Map<Integer, List<Integer>> rules = new HashMap<>();
+    List<List<Integer>> sequence = new ArrayList<>();
+    List<List<Integer>> incorrect = new ArrayList<>();
 
     public void setFilePath(String filePath) {
         this.filePath = filePath;
@@ -26,17 +29,65 @@ public class Day05 {
         System.out.println("Initiating data structures...");
         try {
             input.addAll(Files.readAllLines(Paths.get(filePath)));
-            System.out.println("Finish reading the file. Size: " + input.size());
         } catch (IOException e) {
             System.err.printf("An error occurred while reading the file: %s%n%s%n", e.getMessage(), e);
+        }
+        for (String line: input) {
+            boolean isRule = line.contains("|");
+            if (isRule) {
+                String[] parts = line.split("\\|");
+                int ruleNumber = Integer.parseInt(parts[0]);
+                List<Integer> rule = rules.getOrDefault(ruleNumber, new ArrayList<>());
+                rule.add(Integer.parseInt(parts[1]));
+                rules.put(ruleNumber, rule);
+            } else if (line.contains(",")) {
+                sequence.add(Arrays.stream(line.split(","))
+                        // Convert each substring to an integer
+                        .map(Integer::parseInt)
+                        // Collect the result into a List
+                        .collect(Collectors.toList()));
+            }
         }
     }
 
     public int calculatePart1() {
-        return 0;
+        int res = 0;
+        main: for (List<Integer> seq: sequence) {
+            for (int i = 0; i < seq.size(); i++) {
+                int current = seq.get(i);
+                for (int j = i + 1; j < seq.size(); j++) {
+                    if (rules.containsKey(seq.get(j)) && rules.get(seq.get(j)).contains(current)) {
+                        incorrect.add(seq);
+                        continue main;
+                    }
+                }
+            }
+            int index = seq.size() / 2;
+            res += seq.get(index);
+        }
+        return res;
     }
 
     public int calculatePart2() {
-        return 0;
+        int res = 0;
+        for (List<Integer> seq: incorrect) {
+            main: for (int i = 0; i < seq.size(); i++) {
+                int current = seq.get(i);
+                for (int j = i + 1; j < seq.size(); j++) {
+                    Integer innerNum = seq.get(j);
+                    if (rules.containsKey(innerNum) && rules.get(innerNum).contains(current)) {
+                        int indexCurrent = seq.indexOf(current);
+                        int indexInner = seq.indexOf(innerNum);
+                        seq.set(indexCurrent, innerNum);
+                        seq.set(indexInner, current);
+                        i--;
+                        continue main;
+                    }
+                }
+            }
+            int index = seq.size() / 2;
+            res += seq.get(index);
+        }
+        return res;
     }
 }
